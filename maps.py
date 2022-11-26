1dee8ea775e192e66321090d597dca869cba7ec3
nanoblast <koxigen333@gmail.com>
2022-11-26 14:27:55 +0100

    Implemented basic map without labirynth

---
+++ maps.py
@@ -0,0 +1,21 @@
+class BasicMap(object):
+    map = {}
+
+    def __init__(self) -> None:
+        for x in range(16):
+            for y in range(16):
+                self.map[x+1,y+1] = [
+                    #x-1, y
+                    (max(x, 1), y+1),
+                    #x+1, y
+                    (min(x+2, 16), y+1),
+                    #x, y-1
+                    (x+1, max(y, 1)),
+                    #x, y+1
+                    (x+1, min(y+2, 16))
+                ]
+    
+if  __name__ == "__main__":
+    map = BasicMap()
+    for i in map.map:
+
